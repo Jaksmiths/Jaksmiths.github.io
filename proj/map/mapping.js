87dffabde8f2f18a3e8a89798ea219ab795328ab
Jaksmiths <jaksmith@umich.edu>
2022-01-28 19:05:43 -0500

    added more ui to map

--- proj/map/mapping.js
+++ proj/map/mapping.js
@@ -3,15 +3,39 @@
 
 const sourceData = 'https://raw.githubusercontent.com/Jaksmiths/Jaksmiths.github.io/master/proj/map/Microplastic_Data.json';
 
+showscatter = true;
+showheat = true;
+
+document.getElementById("Scatterplot").onchange = function(){
+  showscatter = showscatter ? false : true;
+};
+document.getElementById("Heatmap").onchange = function(){
+  showheat = showheat ? false : true;
+};
+
 const scatterplot = new deck.ScatterplotLayer({
     id: 'scatter',
     data: sourceData,
-    opacity: 0.8,
+    visible: showscatter,
+    opacity: 1,
     filled: true,
     radiusMinPixels: 3,
     radiusMaxPixels: 5,
     getPosition: d => [d.Long, d.Lat],
-    getFillColor: d => d.PiecesPerKM2 > 0 ? [200, 0, 40, 150] : [255, 140, 0, 100],
+    getFillColor: (d) => {
+      const val = d.PiecesPerKM2;
+      if(val == 0) {
+        return [15, 223, 5, 100];
+      } else if(1000 > val && val >= 100) {
+        return [255, 254, 0, 100];
+      } else if(10000 > val && val >= 1000) {
+        return [255, 108, 0, 100];
+      } else if(50000 > val && val >= 10000) {
+        return [255, 54, 0, 100];
+      } else {
+        return [164, 0, 255, 100];
+      }
+    },
 
     pickable: true,
     onHover: ({object, x, y}) => {
@@ -34,6 +58,7 @@ const scatterplot = new deck.ScatterplotLayer({
 const heatmap = new deck.HeatmapLayer({
     id: 'heat',
     data: sourceData,
+    visible: showheat,
     getPosition: d => [d.Long, d.Lat],
     getWeight: d => d.PiecesPerKM2,
     opacity: 0.6, 
