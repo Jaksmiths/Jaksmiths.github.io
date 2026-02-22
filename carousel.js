49ed2ee0cd971e2b93a4a295f92ccf29320d4b77
Jaksmiths <jaksmith@umich.edu>
2026-02-22 00:11:37 -0500

    updated site to use tailwind and changed text & links

---
+++ carousel.js
@@ -0,0 +1,51 @@
+const carousel = document.getElementById('carousel');
+const cards = [...carousel.children];
+let isDown = false;
+let startX;
+let scrollLeft;
+
+// Create 3 sets: [clones] [originals] [clones]
+cards.forEach(card => carousel.appendChild(card.cloneNode(true)));
+cards.forEach(card => carousel.appendChild(card.cloneNode(true)));
+
+// Start scrolled to the middle set (the originals)
+const singleSetWidth = carousel.scrollWidth / 3 - 50;
+carousel.scrollLeft = singleSetWidth;
+
+// Silently reset when reaching a clone boundary
+carousel.addEventListener('scroll', () => {
+    const setWidth = carousel.scrollWidth / 3;
+    if (carousel.scrollLeft >= setWidth * 2) {
+        carousel.scrollLeft -= setWidth;
+    } else if (carousel.scrollLeft <= 0) {
+        carousel.scrollLeft += setWidth;
+    }
+});
+
+carousel.addEventListener('mousedown', (e) => {
+    isDown = true;
+    carousel.classList.add('cursor-grabbing');
+    carousel.classList.remove('cursor-grab');
+    startX = e.pageX - carousel.offsetLeft;
+    scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseleave', () => {
+    isDown = false;
+    carousel.classList.remove('cursor-grabbing');
+    carousel.classList.add('cursor-grab');
+});
+
+carousel.addEventListener('mouseup', () => {
+    isDown = false;
+    carousel.classList.remove('cursor-grabbing');
+    carousel.classList.add('cursor-grab');
+});
+
+carousel.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - carousel.offsetLeft;
+    const walk = (x - startX) * 2;
+    carousel.scrollLeft = scrollLeft - walk;
+});
--- dark.js
+++ dark.js
@@ -1,11 +1,42 @@
-// Select the button
-// const btn = document.querySelector('.btn-toggle');
+// Intialize <html> with "dark" tag if applicable
+document.documentElement.classList.toggle(
+    "dark",
+    localStorage.theme === "dark" || ( !("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark").matches),
+);
 
-// // Listen for a click on the button
-// btn.addEventListener('click', function() {
-//   // Then toggle (add/remove) the .dark-theme class to the body
-//   document.body.classList.toggle('dark-theme');  
-// })
-function toggle() {
-    document.body.classList.toggle('dark-theme');
+function intializeToggle() {
+    // in Light Mode
+    let symbol = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE --><path fill="currentColor" d="m6.76 4.84l-1.8-1.79l-1.41 1.41l1.79 1.79zM4 10.5H1v2h3zm9-9.95h-2V3.5h2zm7.45 3.91l-1.41-1.41l-1.79 1.79l1.41 1.41zm-3.21 13.7l1.79 1.8l1.41-1.41l-1.8-1.79zM20 10.5v2h3v-2zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6s6-2.69 6-6s-2.69-6-6-6m-1 16.95h2V19.5h-2zm-7.45-3.91l1.41 1.41l1.79-1.8l-1.41-1.41z"/></svg>`;
+    if (!("theme" in localStorage)) {
+        // in Auto mode
+        symbol = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE --><path fill="currentColor" d="M12 22c5.52 0 10-4.48 10-10S17.52 2 12 2S2 6.48 2 12s4.48 10 10 10m1-17.93c3.94.49 7 3.85 7 7.93s-3.05 7.44-7 7.93z"/></svg>`;
+    } else if (localStorage.theme === "dark") {
+        // in Dark mode
+        symbol = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE --><path fill="currentColor" d="M9.5 2c-1.82 0-3.53.5-5 1.35c2.99 1.73 5 4.95 5 8.65s-2.01 6.92-5 8.65c1.47.85 3.18 1.35 5 1.35c5.52 0 10-4.48 10-10S15.02 2 9.5 2"/></svg>`;
+    }
+    document.getElementById("theme").innerHTML = symbol;
 }
+
+function toggle() {
+    let symbol;
+    if (!("theme" in localStorage)) {
+        // in Dark Mode
+        localStorage.theme = "dark";
+        document.documentElement.classList.add("dark");
+        symbol = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE --><path fill="currentColor" d="M9.5 2c-1.82 0-3.53.5-5 1.35c2.99 1.73 5 4.95 5 8.65s-2.01 6.92-5 8.65c1.47.85 3.18 1.35 5 1.35c5.52 0 10-4.48 10-10S15.02 2 9.5 2"/></svg>`;
+    } else if (localStorage.theme === "dark") {
+        // in Light Mode
+        localStorage.theme = "light";
+        document.documentElement.classList.remove("dark");
+        symbol = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE --><path fill="currentColor" d="m6.76 4.84l-1.8-1.79l-1.41 1.41l1.79 1.79zM4 10.5H1v2h3zm9-9.95h-2V3.5h2zm7.45 3.91l-1.41-1.41l-1.79 1.79l1.41 1.41zm-3.21 13.7l1.79 1.8l1.41-1.41l-1.8-1.79zM20 10.5v2h3v-2zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6s6-2.69 6-6s-2.69-6-6-6m-1 16.95h2V19.5h-2zm-7.45-3.91l1.41 1.41l1.79-1.8l-1.41-1.41z"/></svg>`;
+    } else {
+        // in Auto Mode
+        localStorage.removeItem("theme");
+        if (window.matchMedia("(prefers-color-scheme: dark").matches) {
+            document.documentElement.classList.add("dark");
+        }
+        symbol = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE --><path fill="currentColor" d="M12 22c5.52 0 10-4.48 10-10S17.52 2 12 2S2 6.48 2 12s4.48 10 10 10m1-17.93c3.94.49 7 3.85 7 7.93s-3.05 7.44-7 7.93z"/></svg>`;
+    }
+    document.getElementById("theme").innerHTML = symbol;
+}
+
